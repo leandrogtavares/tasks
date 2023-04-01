944ab54144637ec4ac28c9efcdb033b38817d4bc
Leandro Tavares <lgomt@hotmail.com>
2023-04-01 17:01:15 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [tasks, setTasks] = useState<string[]>([])
+
+  const [task, setTask] = useState<string>('')
+
+  function onChangeTextHandler(text: string) : void {
+    setTask(text)
+    console.log('task: ', task);
+  }
+
+  function onAddTaskHandler() : void {
+    setTasks([...tasks, task])
+    console.log('tarefa adicionada: ', task);
+  }
+
+  const onClearHandler = () => {
+    setTask('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={{ marginTop: 30, alignItems: 'center' }}>
+        <Text style={{ fontSize: 24 }}>Tasks</Text>
+        <View style={{ width: '100%' }}>
+          <TextInput
+            style={{ backgroundColor: 'white', height: 40, marginBottom: 10 }}
+            placeholder='add a task'
+            onChangeText={onChangeTextHandler}
+            defaultValue={task}
+          />
+          <Button title="add" onPress={onAddTaskHandler} />
+          <Button title="clear" onPress={onClearHandler } />
+        </View>
+      </View>
+      <View>
+        {tasks.map(t => <Text key={t.toString()}>{t}</Text>)}
+      </View>
     </View>
   );
 }
@@ -13,8 +47,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'lightgreen',
+    justifyContent: 'flex-start',
+
+
   },
 });
